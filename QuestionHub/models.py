c82dd05a7d4a923fdf9c60767ebdb6adc13f245d
Mark Tabak <mtabak@uwaterloo.ca>
2024-01-22 15:46:07 -0500

    Made topic a foreign key of post

--- QuestionHub/models.py
+++ QuestionHub/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+"""
 class User(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=255)
@@ -7,10 +7,15 @@ class User(models.Model):
     num_posts = models.IntegerField(default=0)
     num_comments = models.IntegerField(default=0)
     karma = models.IntegerField(default=0)
+"""
+class Topic(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    topic_name = models.CharField(max_length=255)
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, null=True, blank=True, on_delete = models.CASCADE)
     title = models.CharField(max_length=255)
     body = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -18,7 +23,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+   # author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, null=True, blank=True, on_delete=models.CASCADE)
     parent_comment = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
     is_nested_comment = models.BooleanField(default=False)
@@ -27,7 +32,3 @@ class Comment(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     score = models.IntegerField(default=0)
 
-class Topic(models.Model):
-    id = models.AutoField(primary_key=True, editable=False)
-    topic_name = models.CharField(max_length=255)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
