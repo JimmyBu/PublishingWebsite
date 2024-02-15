e4509cd2986007fd30e5dbc5cdaf01921b5624d5
Mark Tabak <mtabak@uwaterloo.ca>
2024-02-15 18:06:51 -0500

    Added logic for number of posts, comments, and karma

--- QuestionHub/views.py
+++ QuestionHub/views.py
@@ -3,18 +3,35 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 from django.contrib.auth import login, logout
-from .models import Post, Response, Topic, UserProfile
+from .models import Post, Response, Topic, UserProfile, Vote
 from .forms import *
 from django.contrib.auth import get_user_model
+
+#admin
+#Password123!
 
 def Home(request):
     posts = Post.objects.all().order_by("timestamp")
     topics = Topic.objects.all()
+    user_votes = []
+    post_vote_list = []
+    if request.user.is_authenticated:
+        user_votes = Vote.objects.filter(user=request.user)
+        
+        for vote in user_votes:
+          post_vote_list.append(vote.post)
+    
     context = {
         'posts': posts,
-        'topics' : topics,
-        'current_page' : 'post'
+        'topics': topics,
+        'current_page': 'post',
     }
+    
+    if user_votes:
+        context['user_votes'] = user_votes
+    if post_vote_list:
+        context['post_vote_list'] = post_vote_list
+ 
     return render(request, "base.html", context)
 
 
@@ -22,7 +39,14 @@ def post_detail(request, id):
     post = Post.objects.get(id=id)
     comment = CommentForm()
     reply = ReplyForm()
-
+    user_vote = []
+    comment_vote_list = []
+    if request.user.is_authenticated:
+        user_votes = Vote.objects.filter(user=request.user)
+        user_vote = Vote.objects.filter(user=request.user, post=post).first()
+        for vote in user_votes:
+          comment_vote_list.append(vote.comment)
+    
     if request.method == "POST":
         try:
             comment = CommentForm(request.POST)
@@ -31,6 +55,9 @@ def post_detail(request, id):
                 c.user = request.user  # fetch the current user
                 c.post = Post(id=id)  # fetch the current post
                 c.save()  # then save
+                user_profile = UserProfile.objects.get_or_create(user=request.user)[0]
+                user_profile.num_comments += 1
+                user_profile.save()
                 redirect('/post/' + str(id) + '/' + str(c.id))  # parse the post_id and the comment_id
 
         except Exception as e:
@@ -41,12 +68,28 @@ def post_detail(request, id):
         'comment': comment,
         'reply': reply
     }
+    
+    if user_vote:
+        context['user_vote'] = user_vote
+    if user_votes:
+        context['user_votes'] = user_votes
+    if comment_vote_list:
+        context['comment_vote_list'] = comment_vote_list
+    
     return render(request, 'post_detail.html', context)
 
 def topic_detail(request, id):
     topic = Topic.objects.get(id=id)
     filtered_posts = Post.objects.all() if not topic else Post.objects.filter(topic_id=id)
-    topics = Topic.objects.all()  # Assuming you need topics for the dropdown
+    topics = Topic.objects.all()
+    user_votes = []
+    post_vote_list = []
+    if request.user.is_authenticated:
+        user_votes = Vote.objects.filter(user=request.user)
+        
+        for vote in user_votes:
+          post_vote_list.append(vote.post)
+    
     
     context = {
         'filtered_posts': filtered_posts,
@@ -54,6 +97,12 @@ def topic_detail(request, id):
         'current_page' : 'topic_detail',
         'topic' : topic
     }
+    
+    if user_votes:
+        context['user_votes'] = user_votes
+    if post_vote_list:
+        context['post_vote_list'] = post_vote_list
+ 
     return render(request, 'topic_detail.html', context)
 
 def Register(request):
@@ -156,6 +205,9 @@ def create_post(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
+            user_profile = UserProfile.objects.get_or_create(user=request.user)[0]
+            user_profile.num_posts += 1
+            user_profile.save()
             # Redirect to the post detail page of the newly created post
             return redirect('post_detail', id=post.id)
     context = {'form': form}
@@ -182,26 +234,134 @@ def create_topic(request):
 @login_required(login_url='register')
 def upvote_comment(request, pk, vote):
     comment = get_object_or_404(Response, pk=pk)
-    if vote == 'U':
-        comment.score += 1
-    else:
-        comment.score -= 1
-    comment.save()
-
+    user = request.user
+    user_profile = UserProfile.objects.get_or_create(user=comment.user)[0]
     original_url = request.META.get('HTTP_REFERER', '/default/url/')
-    return redirect(original_url)
+    
+    if vote == 'U':
+    # Check if the user has already upvoted this post, if they have undo upvote
+        existing_vote = Vote.objects.filter(user=user, comment=comment).first()
+        if existing_vote and existing_vote.vote_type == 'U':
+            existing_vote.delete()
+            comment.score -= 1
+            user_profile.karma -= 1
+            comment.save()
+            user_profile.save()
+            return redirect(original_url)
+           
+            #If there is an exisitng downvote, remove it, then add a new upvote
+        elif existing_vote and existing_vote.vote_type == 'D':
+            existing_vote.delete()
+            comment.score += 2
+            user_profile.karma += 2
+            comment.save()
+            user_profile.save()
+            Vote.objects.create(user=user, comment=comment, vote_type='U')
+            return redirect(original_url)
+    # Otherwise create a new upvote
+        else:
+            Vote.objects.create(user=user, comment=comment, vote_type='U')
+            comment.score += 1
+            comment.save()
+            user_profile.karma += 1
+            user_profile.save()
+            return redirect(original_url) 
+    #Else vote is downvote
+    else:
+        existing_vote = Vote.objects.filter(user=user, comment=comment).first()
+        #Check if there is an existing downvote, if so undo it
+        if existing_vote and existing_vote.vote_type == 'D':
+            existing_vote.delete()
+            comment.score += 1
+            user_profile.karma += 1
+            comment.save()
+            user_profile.save()
+            return redirect(original_url) 
+        
+        #If there is an exisitng upvote, remove it, then add a downvote
+        elif existing_vote and existing_vote.vote_type == 'U':
+            existing_vote.delete()
+            comment.score -= 2
+            user_profile.karma -= 2
+            comment.save()
+            user_profile.save()
+            Vote.objects.create(user=user, comment=comment, vote_type='D')
+            return redirect(original_url)
+        
+        #Otherwise create a new downvote
+        else:
+            Vote.objects.create(user=user, comment=comment, vote_type='D')
+            comment.score -= 1
+            comment.save()
+            user_profile.karma -= 1
+            user_profile.save()
+            return redirect(original_url) 
 
 @login_required(login_url='register')
 def upvote_post(request, pk, vote):
     post = get_object_or_404(Post, pk=pk)
-    if vote == 'U':
-        post.score += 1
-    else:
-        post.score -= 1
-    post.save()
-
+    user = request.user
+    user_profile = UserProfile.objects.get_or_create(user=post.author)[0]
     original_url = request.META.get('HTTP_REFERER', '/default/url/')
-    return redirect(original_url)
+    
+    if vote == 'U':
+    # Check if the user has already upvoted this post, if they have undo upvote
+        existing_vote = Vote.objects.filter(user=user, post=post).first()
+        if existing_vote and existing_vote.vote_type == 'U':
+            existing_vote.delete()
+            post.score -= 1
+            user_profile.karma -= 1
+            post.save()
+            user_profile.save()
+            return redirect(original_url)
+           
+            #If there is an exisitng downvote, remove it, then add a new upvote
+        elif existing_vote and existing_vote.vote_type == 'D':
+            existing_vote.delete()
+            post.score += 2
+            user_profile.karma += 2
+            post.save()
+            user_profile.save()
+            Vote.objects.create(user=user, post=post, vote_type='U')
+            return redirect(original_url)
+    # Otherwise create a new upvote
+        else:
+            Vote.objects.create(user=user, post=post, vote_type='U')
+            post.score += 1
+            post.save()
+            user_profile.karma += 1
+            user_profile.save()
+            return redirect(original_url) 
+    #Else vote is downvote
+    else:
+        existing_vote = Vote.objects.filter(user=user, post=post).first()
+        #Check if there is an existing downvote, if so undo it
+        if existing_vote and existing_vote.vote_type == 'D':
+            existing_vote.delete()
+            post.score += 1
+            user_profile.karma += 1
+            post.save()
+            user_profile.save()
+            return redirect(original_url) 
+        
+        #If there is an exisitng upvote, remove it, then add a downvote
+        elif existing_vote and existing_vote.vote_type == 'U':
+            existing_vote.delete()
+            post.score -= 2
+            user_profile.karma -= 2
+            post.save()
+            user_profile.save()
+            Vote.objects.create(user=user, post=post, vote_type='D')
+            return redirect(original_url)
+        
+        #Otherwise create a new downvote
+        else:
+            Vote.objects.create(user=user, post=post, vote_type='D')
+            post.score -= 1
+            post.save()
+            user_profile.karma -= 1
+            user_profile.save()
+            return redirect(original_url) 
 
 @login_required(login_url='register')
 def reply_list(request):
@@ -216,9 +376,12 @@ def reply_list(request):
                 r.post = Post(id=post_id)
                 r.parent = Response(id=parent_id)
                 r.save()
+                user_profile = UserProfile.objects.get_or_create(user=request.user)[0]
+                user_profile.num_comments += 1
+                user_profile.save()
                 redirect('/post/' + str(post_id) + '/' + str(r.id))
 
         except Exception as e:
             raise e
 
-    return redirect('home')
+    return redirect('/post/' + str(post_id) + '/' + str(r.id))
