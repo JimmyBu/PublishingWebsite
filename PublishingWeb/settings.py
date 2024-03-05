6f18ec50c0d07284c1d220dd087a67b778906745
Marktabak18 <118449697+Marktabak18@users.noreply.github.com>
2024-03-05 18:38:33 -0500

    Added bootstrap

--- PublishingWeb/settings.py
+++ PublishingWeb/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'QuestionHub',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
